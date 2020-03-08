164520acc549badccc61180626681f4f47a6840b
Watchara Charoenkolakit <watcha2522@gmail.com>
2020-03-08 17:41:19 +0700

    log

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/pagecount', function (req, res) {
       res.send('{ pageCount: ' + count + '}');
     });
   } else {
-    res.send('{ pageCount: -1 }');
+    res.send('{ pageCount: -1, test: true }');
   }
 });
 
